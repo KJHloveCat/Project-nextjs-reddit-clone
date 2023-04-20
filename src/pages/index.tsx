57997905728ad2490f723c7bec1fee3b057643aa
KJHloveCat <kjh99407@gmail.com>
2023-04-20 22:11:14 +0900

    0420

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -138,7 +138,7 @@ export default function Home() {
         postVotes: [],
       }));
     };
-  }, [user, postStateValue]);
+  }, [user, communityStateValue.snippetsFetched]);
 
   return (
     <PageContent>
